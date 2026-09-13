e1aad537b4cc8ceffb33df122e939afbf4d2dc13
nepalhimaladventures431 <nepalhimaladventures431@gmail.com>
2026-09-13 15:52:54 +0530

    further images and boilerpaltes

--- src/app/shop/shop-data.ts
+++ src/app/shop/shop-data.ts
@@ -5,7 +5,38 @@ import { Product } from '../components/ProductCard';
 export const products: Product[] = [
   {
     id: 'the-drop',
+    name: 'Pleated Pants',
+    price: 'NRS 1500',
+    image: '/assets/images/PLEATED1.jpg',
+    hoverImage: '/assets/images/PLEATED2.jpg',
+    href: '/products/the-drop',
+    tag: 'Available Now',
+    sizes: ['S', 'M', 'L', 'XL'],
+  },
+
+  {
+    id: 'the-drop',
     name: 'The Drop Pant',
+    price: 'NRS 1500',
+    image: '/assets/images/1ST.jpeg',
+    hoverImage: '/assets/images/2ND.jpeg',
+    href: '/products/the-drop',
+    tag: 'Available Now',
+    sizes: ['S', 'M', 'L', 'XL'],
+  },
+  {
+    id: 'the-drop',
+    name: 'QUARTER-ZIPS',
+    price: 'NRS 2500',
+    image: '/assets/images/1ST.jpeg',
+    hoverImage: '/assets/images/2ND.jpeg',
+    href: '/products/the-drop',
+    tag: 'Available Now',
+    sizes: ['S', 'M', 'L', 'XL'],
+  },
+  {
+    id: 'the-drop',
+    name: 'Premium Girls Trench Coat',
     price: 'NRS 1500',
     image: '/assets/images/1ST.jpeg',
     hoverImage: '/assets/images/2ND.jpeg',
